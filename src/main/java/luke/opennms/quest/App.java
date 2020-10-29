e4082a2f7db8e42c4850e9cbd11de474eaf74e9e
luke.samad <lsamad@midtowncg.com>
2020-10-28 20:47:32 -0400

    second commit

---
+++ src/main/java/luke/opennms/quest/App.java
@@ -0,0 +1,16 @@
+package luke.opennms.quest;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        SnmpTrapFilter snmpTrapFilter = new SnmpTrapFilter();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Trap Type OID");
+
+        String trapTypeOid = scanner.nextLine();
+        System.out.println("Input: " + trapTypeOid);
+
+        System.out.println("Expected output: " + trapTypeOid + ": " + snmpTrapFilter.hasPrefix(trapTypeOid));
+    }
+}
--- src/main/java/luke/opennms/quest/SnmpTrapFilter.java
+++ src/main/java/luke/opennms/quest/SnmpTrapFilter.java
@@ -10,55 +10,42 @@ import java.util.TreeMap;
 
 public class SnmpTrapFilter {
 
-    static SortedMap<String, String> oidPrefixes = new TreeMap<>();
-    static Integer minPrefixLength = Integer.MAX_VALUE;
+    static private SortedMap<String, String> oidPrefixes = new TreeMap<>();
+    static private Integer minPrefixLength = Integer.MAX_VALUE;
 
-    public void loadOidPrefixes() {
+    public SnmpTrapFilter() {
         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("snmp.yaml");
         Map<String, Object> obj = new Yaml().load(inputStream);
         List<String> prefixes = (List<String>) obj.get("trap-type-oid-prefix");
         for (String prefix : prefixes) {
             oidPrefixes.put(prefix, prefix);
-            int totalParts = prefix.split("\\.").length - 1;
-            if (totalParts < minPrefixLength) {
-                minPrefixLength = totalParts;
+            int parts = prefix.split("\\.").length;
+            if (parts < minPrefixLength) {
+                minPrefixLength = parts;
             }
         }
+        System.out.println("\n\n------------------ snmp.yaml configuration contains ------------------");
         oidPrefixes.keySet().stream().forEach(System.out::println);
-        System.out.println("min: "+minPrefixLength);
+        System.out.println("--------------------------------------------------------------------------");
     }
 
     public boolean hasPrefix(String input) {
-        System.out.println("Testing : "+input);
-        Map<String,String> tailMap = oidPrefixes.tailMap(input);
-        if((!tailMap.isEmpty() && ((SortedMap<String, String>) tailMap).firstKey().startsWith(input))){
-            return true;
-        }else{
-            String [] parts = input.split("\\.");
-            int totalParts = Math.abs(parts.length - minPrefixLength);
-            for (int i = 0; i < totalParts; i++) {
-                System.out.println("Checking Index: "+input);
-                if (oidPrefixes.containsKey(input)) {
-                    return true;
+        if (!oidPrefixes.isEmpty()) {
+            Map<String, String> tailMap = oidPrefixes.tailMap(input);
+            if ((!tailMap.isEmpty() && ((SortedMap<String, String>) tailMap).firstKey().startsWith(input))) {
+                return true;
+            } else {
+                int count = input.split("\\.").length - minPrefixLength;
+                while (count > 0) {
+                    if (oidPrefixes.containsKey(input)) {
+                        return true;
+                    }
+                    input = input.substring(0, input.lastIndexOf('.'));
+                    count--;
                 }
-                input = input.substring(0, input.lastIndexOf('.'));
             }
         }
         return false;
     }
 
-    public static void main(String[] args) {
-        SnmpTrapFilter snmpTrapFilter = new SnmpTrapFilter();
-        snmpTrapFilter.loadOidPrefixes();
-
-        String[] inputs = {
-            ".1.3.6.1.4.1.9.9.117.2.0.1",
-            ".1.3.6.1.4.1.9.9.117",
-            ".1.3.6.1.4.1.9.9.118.2.0.1"
-        };
-        for (String input : inputs) {
-            System.out.println(snmpTrapFilter.hasPrefix(input));
-            System.out.println("---------------------------");
-        }
-    }
 }
