0fe5b18630716098b223fcb8870e290793e141ca
luke.samad <lsamad@midtowncg.com>
2020-10-28 23:01:42 -0400

    fith commit

--- src/main/java/luke/opennms/quest/SnmpTrapFilter.java
+++ src/main/java/luke/opennms/quest/SnmpTrapFilter.java
@@ -13,22 +13,25 @@ import java.util.TreeMap;
 public class SnmpTrapFilter {
 
     static String SNMP_TRAP_CONFIG = "snmp_trap_config";
-    static private SortedMap<String, String> oidPrefixes = new TreeMap<>();
-    static private Integer minPrefixLength = Integer.MAX_VALUE;
+    static private SortedMap<String, String> PREFIXES = new TreeMap<>();
+    static private Integer MIN_PREFIX_LENGTH = Integer.MAX_VALUE;
 
     public SnmpTrapFilter() {
-
         Map<String, Object> obj = new Yaml().load(getSnmpTrapConfigFile());
         List<String> prefixes = (List<String>) obj.get("trap-type-oid-prefix");
         for (String prefix : prefixes) {
-            oidPrefixes.put(prefix, prefix);
+            PREFIXES.put(prefix, prefix);
             int parts = prefix.split("\\.").length;
-            if (parts < minPrefixLength) {
-                minPrefixLength = parts;
+            if (parts < MIN_PREFIX_LENGTH) {
+                MIN_PREFIX_LENGTH = parts;
             }
         }
+        print();
+    }
+
+    private void print(){
         System.out.println("\n\n------------------ snmp.yaml configuration contains ------------------");
-        oidPrefixes.keySet().stream().forEach(System.out::println);
+        PREFIXES.keySet().stream().forEach(System.out::println);
         System.out.println("--------------------------------------------------------------------------");
     }
 
@@ -41,19 +44,19 @@ public class SnmpTrapFilter {
         } catch (FileNotFoundException e) {
             System.err.println("Config file not found at: " + snmpTrapConfig);
         }
-        //default resource folder
+        System.out.println("Loading snmp.yaml from src/main/resources/snmp.yaml");
         return this.getClass().getClassLoader().getResourceAsStream("snmp.yaml");
     }
 
     public boolean hasPrefix(String input) {
-        if (!oidPrefixes.isEmpty()) {
-            Map<String, String> tailMap = oidPrefixes.tailMap(input);
+        if (!PREFIXES.isEmpty()) {
+            Map<String, String> tailMap = PREFIXES.tailMap(input);
             if ((!tailMap.isEmpty() && ((SortedMap<String, String>) tailMap).firstKey().startsWith(input))) {
                 return true;
             } else {
-                int count = input.split("\\.").length - minPrefixLength;
+                int count = input.split("\\.").length - MIN_PREFIX_LENGTH;
                 while (count > 0) {
-                    if (oidPrefixes.containsKey(input)) {
+                    if (PREFIXES.containsKey(input)) {
                         return true;
                     }
                     input = input.substring(0, input.lastIndexOf('.'));
